d08e595387e04f5c6aed56167bbf29eb57a82c04
fo9c <1974555620@qq.com>
2024-07-12 20:03:36 +0800

    init commit

---
+++ src/main/java/cn/fo9c/multimedia_main/MultimediaMainApplication.java
@@ -0,0 +1,35 @@
+package cn.fo9c.multimedia_main;
+
+import com.baomidou.mybatisplus.core.MybatisPlusVersion;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class MultimediaMainApplication {
+
+    public static void main(String[] args) {
+        // 下面获取的是URL的环境变量
+        ConfigurableApplicationContext application = SpringApplication.run(MultimediaMainApplication.class, args);
+        Environment env = application.getEnvironment();
+        String port = env.getProperty("server.port");
+        String property = env.getProperty("server.servlet.context-path");
+        String path = property == null ? "" :  property;
+
+        // 下面获取的是SpringBoot的环境变量
+        String springBootVersion = SpringBootVersion.getVersion();
+
+        // 下面获取的是Mybatis-Plus的环境变量
+        Package pkg = MybatisPlusVersion.class.getPackage();
+        String mybatisPlusVersion = pkg.getImplementationVersion();
+
+        System.out.println(
+                "———————————————————————————————————————————————————————————————————————————————————————————————————————————\n\t" +
+                "         Access URLs:   http://localhost:" + port + path + "              SpringBoot   Version: " + springBootVersion + "\n\t" +
+                "         DataBase:      MySQL                              Mybatis-Plus Version: " + mybatisPlusVersion + "\n" +
+                "———————————————————————————————————————————————————————————————————————————————————————————————————————————");
+    }
+
+}
---
+++ src/test/java/cn/fo9c/multimedia_main/MultimediaMainApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fo9c.multimedia_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
